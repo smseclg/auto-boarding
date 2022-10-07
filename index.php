91ddf5d78dbb04afa14b62f3b98f22eea7f7e686
smseclg <smseclg@gmail.com>
2022-10-07 10:53:52 +0530

    login page content centered

--- index.php
+++ index.php
@@ -2,33 +2,50 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
+
 
 <! --
-display: inline-block;
-    padding: 2px 10px;
-}
-p.error {
-    background: orangered;
-}
+ Multiline comment
 -->
 
+<style>
+
+body {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 0 auto;
+      height: 100vh;
+      background-color: #f1f1f1;
+    }
+.container {
+      #background-color: #2ecc71;
+      width: 200px;
+      height: 200px;
+    }
 </style>
+
 </head>
+
 <body>
 
 <form method="post" action="app/login.php" name="signin-form">
-  <div class="form-element">
+
+  <div class="container">
     <label>Username</label>
     <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
-  </div>
-  <div class="form-element">
+    <br>
     <label>Password</label>
     <input type="password" name="password" required />
+    <br>
+    <br>
+    <button type="submit" name="login" value="login">Log In</button>
+    <button type="reset" name="reset" value="reset">Reset</button>
   </div>
-  <button type="submit" name="login" value="login">Log In</button>
+
 </form>
 
 </body>
+
 </html>
 
